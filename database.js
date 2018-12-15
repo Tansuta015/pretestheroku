8f75608d23440a1e851570bd81a4c09c7e7006c2
tansuta <gilf_512@hotmail.com>
2018-12-15 12:38:04 +0700

    test

--- database.js
+++ database.js
@@ -1,2 +1,2 @@
 const pgp = require('pg-promise');
-var db = pgp('postgres://xufompgyyuqszf:60875effb67559df01a983fd41dc127e7be2339d8693521637a610c514a4bb03@ec2-107-21-125-209.compute-1.amazonaws.com:5432/d49add61t0aqfu?ssl=true');
+var db = pgp('postgres://bjtgkemdffpmvc:8d805ee0eaee39cf0dcc887bf97069a62998afb76cd67fbe60d9394903b7ed0d@ec2-54-83-197-230.compute-1.amazonaws.com:5432/d6vdvpjgevg0kj?ssl=true');
